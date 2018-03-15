bc11188f521e3ea27cdca5986bb0f14c89dc2382
Pooja Srivastava <bioinfor.pooja@gmail.com>
2018-03-15 16:32:30 -0400

    added courseForm and individual coursePage  is rendering

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -3,6 +3,8 @@ import CourseApi from '../api/mockCourseApi';
 
 //this loadCoursesSuccess action will not be fired untill async call successfully
 // happend i.e untill loadCourses(thunk) return successfully thatswhy this action name is LOAD_COURSES_SUCCESS.
+
+//Action 
 export function loadCoursesSuccess(courses) {
 	return {
 		type: types.LOAD_COURSES_SUCCESS,
@@ -11,6 +13,7 @@ export function loadCoursesSuccess(courses) {
 
 }
 
+//THUNK 
 export function loadCourses() {
 	return function(dispatch) {
 		return CourseApi.getAllCourses().then(courses => {
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import AboutPage from './components/about/AboutPage';
 import HomePage from './components/home/HomePage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
 //i.e. on every render, App component will always render. 
@@ -13,6 +14,8 @@ export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} /> 
 		<Route path="courses" component={CoursesPage} />
+		<Route path="course" component={ManageCoursePage} />
+		<Route path="course/:id" component={ManageCoursePage} />
 		<Route path="about" component={AboutPage} />
 	</Route>
 );
