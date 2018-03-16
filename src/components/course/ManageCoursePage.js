9ec9f3903e47266bae1f653ba180928ba6c6f6f1
Pooja Srivastava <bioinfor.pooja@gmail.com>
2018-03-16 14:55:59 -0400

    courseForm is not rendering

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,25 +2,33 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 
 class ManageCoursePage extends React.Component {
- constructor(){
-  super();
+ constructor(props, context){
+  super(props, context);
+  this.state = {
+  	course: Object.assign({}, this.props.course),
+  	error: {}
+  }
  }
  render() {
   return(
-   <h1>Manage Course</h1>
+  	<CourseForm course={this.state.course} />
+   
   );
  }
 }
 
 ManageCoursePage.propTypes = {
+	course: PropTypes.object.isRequired
 
 };
 function mapStateToProps(state, ownProps) {
+let course = {id: '', title: '', watchHref: '', authorId: '', length: '', category: ''}
  return {
-  state: state
+  course: course
  };
 
 }
